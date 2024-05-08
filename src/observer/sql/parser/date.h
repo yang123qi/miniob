c3614d2249772a13d4f140543b4b9d3cb2b33824
yang123qi <131653115+yang123qi@users.noreply.github.com>
2024-05-08 15:18:29 +0800

    Create date.h

---
+++ src/observer/sql/parser/date.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+
+const int kYearBegin = 1970;
+const int kYearEnd = 2038;
+const int kYearCount = kYearEnd - kYearBegin + 1;
+const int kMonthCount = 12;
+const int kSecondsInDay = 24 * 60 * 60;
+
+class Date {
+public:
+  int value;
+  auto operator<=>(const Date &o) const = default;
+  Date(const std::string &s, const std::string &format = "%F");
+  Date(int val) : value(val) {}
+  Date() = default;
+  static std::string to_string(const Date &date, const std::string &format = "%F");
+  static int compare_date(const Date *a, const Date *b);
+};
+const Date INVALID_
+
+
+(-1);
